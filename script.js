ab026f46cf5b8d5e28cd2b5976d6b046f70262b3
KwabenaAmen <kwabenaamen00@gmail.com>
2022-10-31 17:54:35 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var Firstname = prompt("What is your First name?");
+var Lastname = prompt("What is your Last name?");
+var username = Firstname + " " + Lastname
+var Age = prompt('What is your Age?');
+console.log('Your Full name is ' + username);
+console.log('You are ' + Age + ' years old');
+
